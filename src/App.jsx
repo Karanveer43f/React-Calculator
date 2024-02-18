74ea321b25c6e33bd43f057f48178629d1284792
Karanveer <karanveer43f@gmail.com>
2024-02-19 01:56:11 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,38 @@ import Button from "./components/Button";
 function App() {
   const [equation, setEquation] = useState("");
   const [result, setResult] = useState("");
-
+  const buttonsClass =
+    "rounded-xl bg-[#FCF4E5] border-[3px] md:border-2 border-[#2D342C] shadow-[3px_4px_#2D342C] md:shadow-[3px_3px_#2D342C] font-bold uppercase rubik-kv text-3xl md:text-base";
   const evaluate = () => {
     let res = eval(equation);
-    setResult(res);
+
+    // Check if the result is a floating-point number
+    if (Number.isInteger(res)) {
+      // If it's an integer, set it directly
+      setResult(res.toString());
+    } else {
+      // If it's a floating-point number, limit it to three decimal places
+      res = res.toFixed(3);
+      setResult(res);
+    }
   };
 
   const addToEquation = (value) => {
+    //operating on null value
     if (!equation && (value == "*" || value == "/" || value == "+")) {
       window.alert("Can't perform the operation to a Null value");
       return;
     }
+    //using 2 operators together
+    let last = equation[equation.length - 1];
+    if (
+      (last == "*" || last == "/" || last == "+" || last == "-") &&
+      (value == "*" || value == "/" || value == "+" || value == "-")
+    ) {
+      window.alert("Can't operate on two operators");
+      return;
+    }
+
     let tempEquation = equation;
     tempEquation += value;
     setEquation(tempEquation);
@@ -25,136 +46,164 @@ function App() {
     setResult("");
   };
 
+  const erase = () => {
+    if (!equation) {
+      window.alert("Can't erase empty equation");
+      return;
+    }
+    let newEquation = equation.slice(0, -1);
+    setEquation(newEquation);
+  };
+
   return (
     <>
-      <div className="w-full h-full bg-teal-500 fixed grid md:grid-cols-8 md:gap-4 md:grid-rows-12">
-        <div className="md:col-start-4 bg-black col-span-2 rounded-xl row-span-10 md:row-start-2 grid grid-rows-7 ">
-          <div className=" row-span-2 bg-white  m-2 mb-0 rounded-t" id="screen">
-            <div>{result}</div>
-            <div>{equation}</div>
+      <div className="w-full h-full bgw fixed grid md:grid-cols-8 md:grid-rows-12">
+        <div
+          className="md:col-start-4 bg-[#FCF4E5] col-span-2 md:rounded-xl row-span-10 md:row-start-2 grid grid-rows-7 border-4 
+        border-[#2D342C] shadow-[6px_6px_#2D342C] "
+        >
+          <div
+            className=" row-span-2  mb-0 rounded-t-2xl grid grid-rows-3 p-4 text-[#2D342C] rubik-kv"
+            id="screen"
+          >
+            <div className="row-span-2 text-7xl md:text-6xl font-bold flex items-center justify-end">
+              {result}
+            </div>
+            <div className="row-span-1 flex items-center justify-end text-2xl md:text-base ">
+              {" "}
+              {equation}
+            </div>
           </div>
           <div
             id="buttons"
-            className=" row-span-5 bg-red-300 m-2 mt-0 rounded-b grid grid-rows-5 p-3"
+            className=" row-span-5 bg-[#B8F3D7] mt-0 rounded-t-2xl rounded-b-xl grid grid-rows-5 p-3"
           >
-            <div className="grid grid-cols-2 gap-4 pt-2">
+            <div className="grid grid-cols-2 gap-5 pt-4 ">
               <Button
                 value="clear"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   clear(e);
                 }}
               />
               <Button
-                value="/"
+                value="Erase"
+                className={buttonsClass}
                 onButtonClick={(e) => {
-                  addToEquation(e);
+                  erase(e);
                 }}
               />
             </div>
-            <div className="grid grid-cols-4 gap-4 pt-2">
+            <div className="grid grid-cols-4 gap-5 pt-4">
               <Button
                 value="7"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="8"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="9"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
-                value="*"
-                className="rounded"
+                value="/"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
             </div>
-            <div className="grid grid-cols-4 gap-4 pt-2">
+            <div className="grid grid-cols-4 gap-5 pt-4">
               <Button
                 value="4"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="5"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="6"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
-                value="+"
-                className="rounded"
+                value="*"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
             </div>
-            <div className="grid grid-cols-4 gap-4 pt-2">
+            <div className="grid grid-cols-4 gap-5 pt-4">
               <Button
                 value="1"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="2"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
                 value="3"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
-                value="-"
-                className="rounded"
+                value="+"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
             </div>
-            <div className="grid grid-cols-2 pt-2 gap-4">
+            <div className="grid grid-cols-4 gap-5 pt-4">
+              <Button
+                value="Result"
+                className={`col-span-2 ${buttonsClass}`}
+                onButtonClick={(e) => {
+                  evaluate(e);
+                }}
+              />
               <Button
                 value="0"
-                className="rounded"
+                className={buttonsClass}
                 onButtonClick={(e) => {
                   addToEquation(e);
                 }}
               />
               <Button
-                value="Result"
-                className="rounded"
+                className={buttonsClass}
+                value="-"
                 onButtonClick={(e) => {
-                  evaluate(e);
+                  addToEquation(e);
                 }}
               />
             </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,28 @@
 function Button({ value, className, onButtonClick }) {
+  function animateButton(e) {
+    const button = e.target;
+    button.classList.add("clicked"); // Add a class to trigger the animation
+    setTimeout(() => {
+      button.classList.remove("clicked"); // Remove the class after a short delay
+    }, 300); // Adjust the delay as needed
+  }
+  let logo = null;
+  if (value == "Result") logo = <i className="fa-solid fa-equals"></i>;
+  else if (value == "/") logo = <i className="fa-solid fa-divide"></i>;
+  else if (value == "*") logo = <i className="fa-solid fa-xmark"></i>;
+  else if (value == "clear") logo = <i className="fa-solid fa-trash"></i>;
+  else if (value == "Erase")
+    logo = <i className="fa-solid fa-delete-left "></i>;
   return (
     <>
       <button
-        className={`col-span-1 p-3 border-2 border-black ${className}`}
+        className={`col-span-1 p-3 ${className}`}
         onClick={(e) => {
           onButtonClick && onButtonClick(value);
+          animateButton(e);
         }}
       >
-        {value}
+        {logo ? logo : value}
       </button>
     </>
   );
